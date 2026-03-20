9d14d934122417fa37d8e962e70fddc1bc67f196
RobinE <3351271362@qq.com>
2026-03-20 20:58:06 +0800

    Brust test

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -106,15 +106,15 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-  // MX_GPIO_Init();
-  // MX_DMA_Init();
-  // MX_HRTIM1_Init();
-  // MX_I2C3_Init();
-  // MX_SPI1_Init();
-  // MX_SPI3_Init();
-  // MX_USART1_UART_Init();
-  // MX_USB_Device_Init();
-  // MX_TIM1_Init();
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_HRTIM1_Init();
+  MX_I2C3_Init();
+  MX_SPI1_Init();
+  MX_SPI3_Init();
+  MX_USART1_UART_Init();
+  MX_USB_Device_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
   MX_HRTIM1_Init();
   MX_USB_Device_Init();
